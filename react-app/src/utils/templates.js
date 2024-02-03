6ee8e8882ccf141b2c2aafb6dd2d369a631fa74f
Aston Martinez <aston.martinez1614@gmail.com>
2024-02-02 18:14:38 -0600

    started working on templates for books

---
+++ react-app/src/utils/templates.js
@@ -0,0 +1,296 @@
+const templates = [
+  {
+    id: 0,
+    name: "Three-Act Structure",
+    sections: [
+      {
+        name: "Act I",
+        subsections: [
+          {
+            title: "The Beginning",
+            details: [
+              {
+                name: "Opening Scene",
+                description:
+                  "Sets the tone for the story and introduces the protagonist.",
+              },
+              {
+                name: "Setup",
+                description:
+                  "Scenes that introduce the characters' world, introduce supporting characters, and point to changes to come.",
+              },
+              {
+                name: "Inciting Incident",
+                description:
+                  "The surprise moment that turns the protagonist's world upside down and kicks off the main plot.",
+              },
+              {
+                name: "Call to Action",
+                description:
+                  "How do the characters react to the inciting incident? What choice must the protagonist make?",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Act II",
+        subsections: [
+          {
+            title: "Part I: The Middle",
+            details: [
+              {
+                name: "The Choice",
+                description:
+                  "How does the protagonist decide to deal with the problem? What are the stakes?",
+              },
+              {
+                name: "Rising Action / Mounting Problems",
+                description:
+                  "Troubles mount as the conflict kicks into high gear. What keeps the protagonist going?",
+              },
+              {
+                name: "Midpoint / Reversal",
+                description:
+                  "A surprise event that wrecks the protagonist's plan and raises the stakes, often after a temporary triumph.",
+              },
+            ],
+          },
+          {
+            title: "Part 2: After the Midpoint",
+            details: [
+              {
+                name: "More Troubles",
+                description:
+                  "There is fallout from the midpoint, continuing the conflict as problems pile up.",
+              },
+              {
+                name: "Disaster",
+                description:
+                  "A moment when everything goes wrong and the goal appears impossible. The protagonist's darkest hour begins.",
+              },
+              {
+                name: "Turning Point",
+                description:
+                  "New ideas or fresh inspiration offer hope and set up the story's final act.",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Act III",
+        subsections: [
+          {
+            title: "Resolution",
+            details: [
+              {
+                name: "Plan / Resolve",
+                description:
+                  "All the pieces are moved into place for a final climax or showdown. Subplots tie together and the stakes are raised to a peak.",
+              },
+              {
+                name: "Climax",
+                description:
+                  "The protagonist comes face to face with the antagonist or main obstacle and is victorious... or fails.",
+              },
+              {
+                name: "Denouement",
+                description:
+                  "After the climax, the protagonist and/or world have changed. A final scene provides closure to the story.",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "The Hero's Journey Structure",
+    sections: [
+      {
+        name: "Act I",
+        subsections: [
+          {
+            title: "Ordinary World",
+            details: [
+              {
+                name: "Setting the Stage",
+                description:
+                  "The journey has not yet begun. Delves into the ordinary, mundane reality of our Hero",
+              },
+              {
+                name: "Character Introductions",
+                description:
+                  "Establishes a connection by portraying the Hero as a 'normal' person in a 'normal' setting.",
+              },
+            ],
+          },
+          {
+            title: "Call to Adventure",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Refuse the Call",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Meet the Mentor",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Act II",
+        subsections: [
+          {
+            title: "Cross the First Threshold",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Tests, Allies, Enemies",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Innermost Cave",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Ordeal",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Reward",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Act III",
+        subsections: [
+          {
+            title: "The Road Back",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+          {
+            title: "Return with the Elixir",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "",
+    sections: [
+      {
+        name: "",
+        subsections: [
+          {
+            title: "",
+            details: [
+              {
+                name: "",
+                description: "",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default templates;
