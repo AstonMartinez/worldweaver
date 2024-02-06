e2d477d8557e9bb180db805fd2b9c3a3ad67e360
Aston Martinez <aston.martinez1614@gmail.com>
2024-02-06 14:47:13 -0600

    added section to world questionnaire

---
+++ react-app/src/utils/Questionnaires/World/WorldAstronomyPlanets.js
@@ -0,0 +1,10 @@
+const worldAstronomyPlanets = [
+  "How many planets are in this world? What are they called?",
+  "What is unique about each planet's orbit or composition?",
+  "Is there life on other planets, and if so, what does it look like?",
+  "How many moons orbit each planet? Do they have names?",
+  "What are the physical characteristics of each planet and moon?",
+  "Do any moons have atmospheres, water, or geological activity?",
+];
+
+export default worldAstronomyPlanets;
