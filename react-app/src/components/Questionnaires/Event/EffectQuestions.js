418be195f293aaf731aedcf10434ebc0e05acfda
Aston Martinez <aston.martinez1614@gmail.com>
2024-02-19 15:03:16 -0600

    finished adding event questionnaire

--- react-app/src/components/Questionnaires/Event/EffectQuestions.js
+++ react-app/src/components/Questionnaires/Event/EffectQuestions.js
@@ -2,9 +2,113 @@ import { useState } from "react";
 import EventQuestionnaire from "../../../utils/Questionnaires/Event/Combined";
 
 const EffectQuestions = ({ setEffects }) => {
+  const [inputs, setInputs] = useState({
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: "",
+    six: "",
+    seven: "",
+  });
+
+  const handleInputChange = async (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <>
-      <div></div>
+      <div>
+        <h3>Effect</h3>
+      </div>
+      <div>
+        <section>
+          <div>
+            <label htmlFor="one">{EventQuestionnaire.Effect[0]}</label>
+          </div>
+          <div>
+            <textarea
+              name="one"
+              value={inputs.one}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="two">{EventQuestionnaire.Effect[1]}</label>
+          </div>
+          <div>
+            <textarea
+              name="two"
+              value={inputs.two}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="three">{EventQuestionnaire.Effect[2]}</label>
+          </div>
+          <div>
+            <textarea
+              name="three"
+              value={inputs.three}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="four">{EventQuestionnaire.Effect[3]}</label>
+          </div>
+          <div>
+            <textarea
+              name="four"
+              value={inputs.four}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="five">{EventQuestionnaire.Effect[4]}</label>
+          </div>
+          <div>
+            <textarea
+              name="five"
+              value={inputs.five}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="six">{EventQuestionnaire.Effect[5]}</label>
+          </div>
+          <div>
+            <textarea
+              name="six"
+              value={inputs.six}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="seven">{EventQuestionnaire.Effect[6]}</label>
+          </div>
+          <div>
+            <textarea
+              name="seven"
+              value={inputs.seven}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+      </div>
+      <div>
+        <button onClick={() => setEffects(inputs)}>Save & Continue</button>
+      </div>
     </>
   );
 };
--- react-app/src/components/Questionnaires/Event/ThemeQuestions.js
+++ react-app/src/components/Questionnaires/Event/ThemeQuestions.js
@@ -2,9 +2,61 @@ import { useState } from "react";
 import EventQuestionnaire from "../../../utils/Questionnaires/Event/Combined";
 
 const ThemeQuestions = ({ setThemes }) => {
+  const [inputs, setInputs] = useState({
+    one: "",
+    two: "",
+    three: "",
+  });
+
+  const handleInputChange = async (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <>
-      <div></div>
+      <div>
+        <h3>Themes</h3>
+      </div>
+      <div>
+        <section>
+          <div>
+            <label htmlFor="one">{EventQuestionnaire.Cause[0]}</label>
+          </div>
+          <div>
+            <textarea
+              name="one"
+              value={inputs.one}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="two">{EventQuestionnaire.Cause[1]}</label>
+          </div>
+          <div>
+            <textarea
+              name="two"
+              value={inputs.two}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <label htmlFor="three">{EventQuestionnaire.Cause[2]}</label>
+          </div>
+          <div>
+            <textarea
+              name="three"
+              value={inputs.three}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+        </section>
+      </div>
+      <div>
+        <button onClick={() => setThemes(inputs)}>Save & Continue</button>
+      </div>
     </>
   );
 };
